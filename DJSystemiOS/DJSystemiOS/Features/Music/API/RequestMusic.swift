3817086dd00cfa66d917eb439b2358400b6b4b38
txxxxc <tomoya113.code@gmail.com>
2023-03-04 02:41:07 +0900

    [add] Features/Music/API/RequestMusic

---
+++ DJSystemiOS/DJSystemiOS/Features/Music/API/RequestMusic.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension Room.API {
+    struct RequestMusicInput {
+        let musics: [String]
+        let radioName: String
+        let message: String
+    }
+    struct RequestMusicResponse: Codable {
+        let ok: Bool
+    }
+}
+
+protocol RequestMusicProtocol {
+    func requestMusic(input: Room.API.RequestMusicResponse) async throws -> Room.API.RequestMusicResponse
+}
+
+extension Room.API: RequestMusicProtocol {
+    func requestMusic(input: Room.API.RequestMusicResponse) async throws -> Room.API.RequestMusicResponse {
+        return Room.API.RequestMusicResponse(ok: false)
+    }
+    
+}
