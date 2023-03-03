feb8bbb5984529b784d5c6a7ee5a5611535fbce3
txxxxc <tomoya113.code@gmail.com>
2023-03-04 02:41:40 +0900

    [add] RequestMusicViewController

---
+++ DJSystemiOS/DJSystemiOS/Pages/RequestMusicPage/RequestMusicViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import SwiftUI
+
+class RequestMusicViewController: UIViewController {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let requestMusicView = RequestMusicView()
+        let hostingVC = UIHostingController(rootView: requestMusicView)
+        addChild(hostingVC)
+        view.addSubview(hostingVC.view)
+        hostingVC.didMove(toParent: self)
+        hostingVC.coverView(parent: view)
+    }
+
+}
