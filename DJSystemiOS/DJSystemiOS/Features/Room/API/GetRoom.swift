9e52860071d2e4acbec94ee96b96e03260c7ae7c
txxxxc <tomoya113.code@gmail.com>
2023-03-04 02:34:37 +0900

    [update] Room/API/GetRoom

--- DJSystemiOS/DJSystemiOS/Features/Room/API/GetRoom.swift
+++ DJSystemiOS/DJSystemiOS/Features/Room/API/GetRoom.swift
@@ -1,11 +1,11 @@
 import Foundation
 import Moya
 
-protocol RoomAPIProtocol {
+protocol GetRoomAPIProtocol {
     func getRoom(id: String) async throws -> RoomOverview
 }
 
-extension Room.API: RoomAPIProtocol {
+extension Room.API: GetRoomAPIProtocol {
     func getRoom(id: String) async throws -> RoomOverview {
         return try await withCheckedThrowingContinuation { continuation in
             Room.API.provider.request(.getRoom(id: id)) { result in
