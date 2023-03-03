fef3ada137eb75fdbb5e774f3c418e2fe7060e3b
txxxxc <tomoya113.code@gmail.com>
2023-03-04 02:41:27 +0900

    [add] RequestMusicView

---
+++ DJSystemiOS/DJSystemiOS/Pages/RequestMusicPage/RequestMusicView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RequestMusicView: View {
+    var body: some View {
+        Text("RequestMusicView")
+    }
+}
+
+struct RequestMusicView_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestMusicView()
+    }
+}
