27ad15733392244f12307e65d0ff5d85b2eec595
txxxxc <tomoya113.code@gmail.com>
2023-04-17 17:10:03 +0900

    [add] CooltimeService

--- DJSystemiOS/Features/Room/Service/CooltimeService.swift
+++ DJSystemiOS/Features/Room/Service/CooltimeService.swift
@@ -1,58 +1,52 @@
 import UIKit
 
 struct CooltimeService {
-    static let COOLTIME_KEY = "COOLTIME"
-    func getCooltime() -> String? {
-        let cooltime = fetchCooltime()
+    private let cooltimeDataSource: CooltimeDataSourceProtocol
+
+    init(dataSource: CooltimeDataSourceProtocol) {
+        self.cooltimeDataSource = dataSource
+    }
+
+    func getRemainingCooltime() -> String? {
+        if hasExpired { return nil }
+        let cooltime = try? cooltimeDataSource.fetch()
         guard let cooltime = cooltime else { return nil }
-        let currentTime = Date().timeIntervalSince1970
+        let (hours, minutes, seconds) = calculateTimeLeft(cooltime: Int(cooltime))
+        let formattedTime = formatTime(
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        )
+        return formattedTime
+    }
+
+    func calculateTimeLeft(cooltime: Int) -> (Int, Int, Int) {
+        let currentTime = Int(Date().timeIntervalSince1970)
         let timeDifference = cooltime - currentTime
         let hours = Int(timeDifference) / 3_600
         let minutes = (Int(timeDifference) % 3_600) / 60
         let seconds = Int(timeDifference) % 60
+        return (hours, minutes, seconds)
+    }
 
+    func formatTime(hours: Int, minutes: Int, seconds: Int) -> String {
         if hours > 0 {
-            let formattedTime = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-            return formattedTime
+            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
         }
-
-        let formattedTime = String(format: "%02d:%02d", minutes, seconds)
-        return formattedTime
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 
     func saveCooltime(unixTime: TimeInterval) {
-        UserDefaults.standard.set(time, forKey: Self.COOLTIME_KEY)
+        cooltimeDataSource.set(unixTime: unixTime)
     }
 
     func removeExpiredCooltime() {
-        UserDefaults.standard.removeObject(forKey: Self.COOLTIME_KEY)
-    }
-
-    func fetchCooltime() -> TimeInterval? {
-        let cooltime = UserDefaults.standard.value(forKey: Self.COOLTIME_KEY)
-        guard let cooltime = cooltime else {
-            // TODO: print以外の方法探したい
-            print("Key is not found")
-            return nil
-        }
-        guard let cooltime = cooltime as? TimeInterval else {
-            print("Value is not equivalent to TimeInterval")
-            return nil
-        }
-        return cooltime
+        cooltimeDataSource.remove()
     }
 
     var hasExpired: Bool {
-        let cooltime = UserDefaults.standard.value(forKey: Self.COOLTIME_KEY)
-        guard let cooltime = cooltime else {
-            // TODO: print以外の方法探したい
-            print("Key is not found")
-            return false
-        }
-        guard let cooltime = cooltime as? TimeInterval else {
-            print("Value is not equivalent to TimeInterval")
-            return false
-        }
+        let cooltime = try? cooltimeDataSource.fetch()
+        guard let cooltime = cooltime else { return true }
         let currentTime = Date().timeIntervalSince1970
         return cooltime <= currentTime
     }
