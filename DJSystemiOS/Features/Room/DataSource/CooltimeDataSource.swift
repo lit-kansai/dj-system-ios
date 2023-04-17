65cd00201db1a76c28e1f199e3ad6b08c8a42a8e
txxxxc <tomoya113.code@gmail.com>
2023-04-17 17:09:40 +0900

    [add] CooltimeDataSource

--- DJSystemiOS/Features/Room/DataSource/CooltimeDataSource.swift
+++ DJSystemiOS/Features/Room/DataSource/CooltimeDataSource.swift
@@ -1,8 +1,31 @@
-//
-//  CooltimeDataSource.swift
-//  DJSystemiOS
-//
-//  Created by Tomoya Tanaka on 2023/04/14.
-//
+import UIKit
 
-import Foundation
+
+protocol CooltimeDataSourceProtocol {
+    func fetch() throws -> TimeInterval
+    func remove()
+    func set(unixTime: TimeInterval)
+}
+
+class CooltimeDataSource: CooltimeDataSourceProtocol {
+
+    static let COOLTIME_KEY = "COOLTIME"
+    func fetch() throws -> TimeInterval {
+        let cooltime = UserDefaults.standard.value(forKey: Self.COOLTIME_KEY)
+        guard let cooltime = cooltime else {
+            throw CooltimeDataSourceError.dataNotFound
+        }
+        guard let cooltime = cooltime as? TimeInterval else {
+            throw CooltimeDataSourceError.wrongValueType
+        }
+        return cooltime
+    }
+
+    func remove() {
+        UserDefaults.standard.removeObject(forKey: Self.COOLTIME_KEY)
+    }
+
+    func set(unixTime: TimeInterval) {
+        UserDefaults.standard.set(unixTime, forKey: Self.COOLTIME_KEY)
+    }
+}
