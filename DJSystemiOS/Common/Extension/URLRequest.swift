236cbc8dc879aea3cf7a12e47afc891ccc36d065
txxxxc <tomoya113.code@gmail.com>
2023-04-29 13:38:54 +0900

    [update] URLRequest

--- DJSystemiOS/Common/Extension/URLRequest.swift
+++ DJSystemiOS/Common/Extension/URLRequest.swift
@@ -4,6 +4,7 @@ extension URLRequest {
     init(url: URL, requestBody: Data) {
         self.init(url: url)
         self.httpMethod = "POST"
+        self.addValue("application/json", forHTTPHeaderField: "Content-Type")
         self.httpBody = requestBody
     }
 }
