deda3d84c09559c6bc687aa3039bd8f585e8f38a
silli-3072 <hrtmnr.3072@gmail.com>
2023-05-20 11:24:43 +0900

    update: cooltime

--- DJSystemiOS/Pages/CooltimePage/CooltimeViewController.swift
+++ DJSystemiOS/Pages/CooltimePage/CooltimeViewController.swift
@@ -2,23 +2,67 @@ import UIKit
 
 class CooltimeViewController: UIViewController {
 
-    /*TODO:
-     画面がロードされたときにCooltimeServiceからクールタイムを取得し表示する
-     1秒間隔でCooltimeServiceから残り時間を取得し、更新する
-     クールタイムが0になった時、なったらタイマーを止める
-     */
+    @IBOutlet var timerLabel: UILabel!
+    
+    let cooltimeService = CooltimeService(dataSource: CooltimeDataSource())
+
+    var coolTimeTimer = Timer()
+    var time = Date().timeIntervalSince1970
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let cooltimeService = CooltimeService(dataSource: CooltimeDataSource())
-
-        // 残り時間を取得する
         let remainingTime = cooltimeService.getRemainingCooltime()
-        // 残り時間をUserDefaultsを保存する
-        //    https://wa3.i-3-i.info/word18474.html
-        let time = Date().timeIntervalSince1970 + 1000
+        
+        timerLabel.text = remainingTime
+        
+        timer()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
         cooltimeService.saveCooltime(unixTime: time)
+        
+    }
+
+    func timer() {
+        coolTimeTimer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(down(timer:)),
+            userInfo: nil,
+            repeats: true
+        )
+
+    }
+
+    @objc func down(timer: Timer) {
+        let remainingTime = cooltimeService.getRemainingCooltime()
+        
+        time -= 1
+        
+        let hours = Int(time) / 3_600
+        let minutes = (Int(time) % 3_600) / 60
+        let seconds = Int(time) % 60
+        
+        if seconds < 10 {
+            timerLabel.text = String("\(minutes):0\(seconds)")
+        } else {
+            timerLabel.text = String("\(minutes):\(seconds)")
+        }
+        
+        if time == 0 {
+            timer.invalidate()
+        }
+        
     }
 
 }
+
+
+/*TODO:
+ 画面がロードされたときにCooltimeServiceからクールタイムを取得し表示する
+ 1秒間隔でCooltimeServiceから残り時間を取得し、更新する
+ クールタイムが0になった時、なったらタイマーを止める
+ */
