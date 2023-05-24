64728c1a2652a214d1016a9a5fc9eb3690838b65
txxxxc <tomoya113.code@gmail.com>
2023-05-24 14:13:54 +0900

    add: Color Extension

---
+++ DJSystemiOS/Common/Extension/Color/Color+init.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Color {
+    init(
+        light lightModeColor: Color,
+        dark darkModeColor: Color
+    ) {
+        self.init(UIColor(
+            light: UIColor(lightModeColor),
+            dark: UIColor(darkModeColor)
+        ))
+    }
+}
+
+
