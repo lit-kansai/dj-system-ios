81d2ed253f3f2f99ffbc0e68ce4d7cba31382399
txxxxc <tomoya113.code@gmail.com>
2023-05-24 14:13:44 +0900

    add: UIColor+init

---
+++ DJSystemiOS/Common/Extension/UIColor/UIColor+init.swift
@@ -0,0 +1,34 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+    }
+}
+
+extension UIColor {
+    convenience init(
+        light lightModeColor: @escaping @autoclosure () -> UIColor,
+        dark darkModeColor: @escaping @autoclosure () -> UIColor
+     ) {
+        self.init { traitCollection in
+            switch traitCollection.userInterfaceStyle {
+            case .light:
+                return lightModeColor()
+            case .dark:
+                return darkModeColor()
+            case .unspecified:
+                return lightModeColor()
+            @unknown default:
+                return lightModeColor()
+            }
+        }
+    }
+}
+
+
