a17124d8b6983eefb081ed47852a7f7cea401bcc
silli-3072 <hrtmnr.3072@gmail.com>
2023-05-29 14:45:06 +0900

    fix: userdefaultを使用する方法に変更

--- DJSystemiOS/Pages/CooltimePage/CooltimeViewController.swift
+++ DJSystemiOS/Pages/CooltimePage/CooltimeViewController.swift
@@ -5,32 +5,21 @@ class CooltimeViewController: UIViewController {
     @IBOutlet private var timerLabel: UILabel!
 
     let cooltimeService = CooltimeService(dataSource: CooltimeDataSource())
-
-    var time = Date().timeIntervalSince1970 + 300
+    let cooltimeDataSource = CooltimeDataSource()
 
     var coolTimeTimer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var setTime = cooltimeService.getRemainingCooltime()
+        let leftTime = cooltimeService.getRemainingCooltime()
+        timerLabel.text = leftTime
 
-        if setTime == nil {
-            var setTime = cooltimeService.calculateTimeLeft(cooltime: Int(time))
-            var text = cooltimeService.formatTime(hours: setTime.0, minutes: setTime.1, seconds: setTime.2)
-            timerLabel.text = text
-        } else {
-            timerLabel.text = setTime
+        guard let leftTime = leftTime else {
+            return
         }
 
         timer()
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-
-        cooltimeService.saveCooltime(unixTime: time)
-
     }
 
     func timer() {
@@ -46,20 +35,17 @@ class CooltimeViewController: UIViewController {
 
     @objc
     func countDown() {
-        var setTime = cooltimeService.calculateTimeLeft(cooltime: Int(time))
-        var text = cooltimeService.formatTime(hours: setTime.0, minutes: setTime.1, seconds: setTime.2)
+        let savedTime = try! cooltimeDataSource.fetch()
+        let setTime = cooltimeService.calculateTimeLeft(cooltime:Int(savedTime))
+        let text = cooltimeService.formatTime(hours: setTime.0, minutes: setTime.1, seconds: setTime.2)
+
+        var timerCheck = cooltimeService.hasExpired
+
         timerLabel.text = text
 
-        if text == "00:00" {
+        if timerCheck == true {
             coolTimeTimer.invalidate()
         }
     }
 
 }
-
-
-/*TODO:
- 画面がロードされたときにCooltimeServiceからクールタイムを取得し表示する
- 1秒間隔でCooltimeServiceから残り時間を取得し、更新する
- クールタイムが0になった時、なったらタイマーを止める
- */
