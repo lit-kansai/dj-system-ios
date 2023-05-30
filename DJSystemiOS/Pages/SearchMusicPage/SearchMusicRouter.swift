0e824a0eb4c8d261e66ab70d82506de4e6ed1b30
txxxxc <tomoya113.code@gmail.com>
2023-05-30 18:43:37 +0900

    update: SearchMusic page

--- DJSystemiOS/Pages/SearchMusicPage/SearchMusicRouter.swift
+++ DJSystemiOS/Pages/SearchMusicPage/SearchMusicRouter.swift
@@ -6,8 +6,9 @@ protocol SearchMusicRouterProtocol: AnyObject {
 
 final class SearchMusicRouter: SearchMusicRouterProtocol {
     func transitionToRequestMusicPage(_ navigationController: UINavigationController, roomId: String, music: Music) {
+        let cooltimeSerivce = CooltimeService(dataSource: CooltimeDataSource())
         // 遷移先のRoomOverViewController
-        let requestMusicViewController = RequestMusicViewController(roomId: roomId, music: music)
+        let requestMusicViewController = RequestMusicViewController(cooltimeService: cooltimeSerivce, roomId: roomId, music: music)
         // 画面遷移
         navigationController.pushViewController(requestMusicViewController, animated: true)
     }
