2ead16af10b4cf8e9051ac84d59e3cef17b83c8d
silli-3072 <hrtmnr.3072@gmail.com>
2023-06-04 09:38:59 +0900

    fix: cooltimeの実装（falseで返ってくる）

--- DJSystemiOS/Pages/CooltimePage/CooltimeViewController.swift
+++ DJSystemiOS/Pages/CooltimePage/CooltimeViewController.swift
@@ -5,45 +5,44 @@ class CooltimeViewController: UIViewController {
     @IBOutlet private var timerLabel: UILabel!
 
     let cooltimeService = CooltimeService(dataSource: CooltimeDataSource())
-    let cooltimeDataSource = CooltimeDataSource()
 
     var coolTimeTimer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let leftTime = cooltimeService.getRemainingCooltime()
-        timerLabel.text = leftTime
+        updateCooltimeLabel()
 
-        guard let leftTime = leftTime else {
-            return
+        if let _ = cooltimeService.getRemainingCooltime() {
+            timer()
         }
 
-        timer()
     }
 
     func timer() {
         coolTimeTimer = Timer.scheduledTimer(
             timeInterval: 1.0,
             target: self,
-            selector: #selector(self.countDown),
+            selector: #selector(countDown),
             userInfo: nil,
             repeats: true
         )
 
     }
 
+    func updateCooltimeLabel() {
+        if let leftTime = cooltimeService.getRemainingCooltime() {
+            timerLabel.text = leftTime
+        } else {
+            return
+        }
+    }
+
     @objc
     func countDown() {
-        let savedTime = try! cooltimeDataSource.fetch()
-        let setTime = cooltimeService.calculateTimeLeft(cooltime:Int(savedTime))
-        let text = cooltimeService.formatTime(hours: setTime.0, minutes: setTime.1, seconds: setTime.2)
+        updateCooltimeLabel()
 
-        var timerCheck = cooltimeService.hasExpired
-
-        timerLabel.text = text
-
-        if timerCheck == true {
+        if cooltimeService.hasExpired {
             coolTimeTimer.invalidate()
         }
     }
