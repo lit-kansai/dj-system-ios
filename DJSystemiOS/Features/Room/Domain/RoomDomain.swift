a9a3b80de4efe246bb9a3ad61ee01db8924c3165
txxxxc <tomoya113.code@gmail.com>
2023-06-13 20:41:00 +0900

    add: RoomHistory

--- DJSystemiOS/Features/Room/Domain/RoomDomain.swift
+++ DJSystemiOS/Features/Room/Domain/RoomDomain.swift
@@ -5,3 +5,20 @@ struct RoomOverview: Codable {
     let name: String
     let description: String
 }
+
+// Realm用にclassで作ってます
+final class RoomHistory {
+
+    let id: String
+    let name: String
+    let description: String
+    let emojiIcon: Character
+
+    init(id: String, name: String, description: String, emojiIcon: Character) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.emojiIcon = emojiIcon
+    }
+
+}
