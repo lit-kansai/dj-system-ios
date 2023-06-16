c5784eceada598bef00a9a0281eff02ca38fc9b3
txxxxc <tomoya113.code@gmail.com>
2023-06-16 14:12:30 +0900

    update: AppConfig

--- DJSystemiOS/Common/AppConfig.swift
+++ DJSystemiOS/Common/AppConfig.swift
@@ -1,7 +1,31 @@
 import Foundation
 
-enum AppConfig {
-    static var BaseAPIURL: URL {
-        return URL(string: "https://stg-dj-api.life-is-tech.com")!
+protocol AppConfigProtocol {
+    var environment: Environment { get }
+    var BaseAPIURL: URL { get }
+}
+
+enum Environment: String {
+    case debug = "DEBUG"
+    case release = "RELEASE"
+}
+
+struct AppConfig: AppConfigProtocol {
+    var environment: Environment {
+        guard let environmentString = Bundle.main.object(forInfoDictionaryKey: "Environment") as? String,
+              let environment = Environment(rawValue: environmentString)
+        else {
+            fatalError("Fail to load `Environment` from `Info.plist`.")
+        }
+        return environment
+    }
+
+    var BaseAPIURL: URL {
+        switch environment {
+        case .debug:
+            return URL(string: "https://stg-dj-api.life-is-tech.com")!
+        case .release:
+            return URL(string: "https://dj-api.life-is-tech.com")!
+        }
     }
 }
