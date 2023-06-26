2eb2f2c8aa1184bda0dbf84f8db6fb3274fa2958
txxxxc <tomoya113.code@gmail.com>
2023-06-26 12:20:25 +0900

    update: document

--- DJSystemiOS/Common/Extension/URL/URL+extractRoomID.swift
+++ DJSystemiOS/Common/Extension/URL/URL+extractRoomID.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+// QRから読んだURLからroomIDだけ正規表現で抜き出す
+// Result<String, InvalidURLError>型で返ってくるので良い感じに使ってくださいー
+// let result = URL.extractRoomID(inputURL: "https://dj-life-is-tech.com/sample-gassi")
+// print(result) // .success("sample-gassi")
 extension URL {
     static func extractRoomID(inputURL: String) -> Result<String, InvalidURLError> {
         let result = inputURL.capture(pattern: RegularExpression.roomURL, group: 1)
--- DJSystemiOS/Pages/QRReaderPage/QRReaderViewController.swift
+++ DJSystemiOS/Pages/QRReaderPage/QRReaderViewController.swift
@@ -3,19 +3,10 @@ import UIKit
 
 final class QRReaderViewController: UIViewController {
 
-    // これ読みながらQRコードを読むやつやって欲しい: https://github.com/mercari/QRScanner
-    // QRScannerはインストール済みなので導入するところから始めてくださいー！
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 //    private func extractRoomID() {
-        // QRから読んだURLからroomIDだけ正規表現で抜き出す
-        // Result<String, InvalidURLError>型で返ってくるので良い感じに使ってくださいー
-        // let result = URL.extractRoomID(inputURL: "https://dj-life-is-tech.com/sample-gassi")
-        // print(result) // .success("result")
-
-//    }
 
 }
