0a8dc615f4ca32e6f931d91bc67e93f756d0e991
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2023-07-21 22:59:00 +0900

    add:read QR code and check QR code

--- DJSystemiOS/Pages/QRReaderPage/QRReaderViewController.swift
+++ DJSystemiOS/Pages/QRReaderPage/QRReaderViewController.swift
@@ -1,3 +1,4 @@
+import AVFoundation
 import QRScanner
 import UIKit
 
@@ -5,8 +6,59 @@ final class QRReaderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupQRScanner()
     }
 
-//    private func extractRoomID() {
+    private func setupQRScanner() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            setupQRScannerView()
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                if granted {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.setupQRScannerView()
+                    }
+                }
+            }
+        default:
+            showAlert()
+        }
+    }
 
+    private func setupQRScannerView() {
+        let qrScannerView = QRScannerView(frame: view.bounds)
+        view.addSubview(qrScannerView)
+        qrScannerView.configure(delegate: self, input: .init(isBlurEffectEnabled: true))
+        qrScannerView.startRunning()
+    }
+
+    private func showAlert() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+            let alert = UIAlertController(title: "Error", message: "Camera is required to use in this application", preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default))
+            self?.present(alert, animated: true)
+        }
+    }
+}
+
+extension QRReaderViewController: QRScannerViewDelegate {
+    func qrScannerView(_ qrScannerView: QRScannerView, didFailure error: QRScannerError) {
+        print(error)
+        let alert = UIAlertController(title: "Error", message: "This QR Code is invalid", preferredStyle: .alert)
+        alert.addAction(.init(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+
+    func qrScannerView(_ qrScannerView: QRScannerView, didSuccess code: String) {
+        let requestedRoomID = URL.extractRoomID(inputURL: code)
+        switch requestedRoomID {
+        case .success(let str) :
+            let validURL = str
+        case .failure :
+            let alert = UIAlertController(title: "Error", message: "No room found for this URL", preferredStyle: .alert)
+            alert.addAction(.init(title: "OK", style: .default))
+            self.present(alert, animated: true)
+        }
+    }
 }
